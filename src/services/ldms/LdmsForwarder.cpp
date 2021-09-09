f964df3bfd694a2c8223523d5f6efceb91809480
David Boehme <boehme3@llnl.gov>
2021-09-09 01:34:25 +0000

    Add initial LdmsForwarder demo

---
+++ src/services/ldms/LdmsForwarder.cpp
@@ -0,0 +1,102 @@
+// Copyright (c) 2021, Lawrence Livermore National Security, LLC.
+// See top-level LICENSE file for details.
+
+#include "caliper/CaliperService.h"
+
+#include "caliper/Caliper.h"
+#include "caliper/RegionProfile.h"
+#include "caliper/SnapshotRecord.h"
+
+#include "caliper/common/Log.h"
+#include "caliper/common/OutputStream.h"
+#include "caliper/common/RuntimeConfig.h"
+
+#include "../../common/util/format_util.h"
+
+#include <map>
+#include <string>
+
+using namespace cali;
+
+namespace
+{
+
+std::ostream& write_to_json(std::ostream& os, std::map<std::string, double> region_times) {
+    os << "{ ";
+
+    int c = 0;
+    for (auto p : region_times)
+        util::write_esc_string(os << (c++ > 0 ? ", \"" : "\""), p.first) << "\": " << p.second;
+
+    return os << " }";
+}
+
+class LdmsForwarder
+{
+    static const ConfigSet::Entry s_configdata[];
+
+    RegionProfile profile;
+    OutputStream  stream;
+
+    std::string   filename;
+
+    void snapshot(Caliper*, Channel*) {
+        std::map<std::string, double> region_times;
+
+        std::tie(region_times, std::ignore, std::ignore) =
+            profile.inclusive_region_times();
+
+        write_to_json(*stream.stream(), region_times) << "\n";
+
+        profile.clear(); // reset profile - skip to create a cumulative profile
+    }
+
+    void post_init(Caliper* c, Channel* channel) {
+        std::vector<Entry> rec;
+        stream.set_filename(filename.c_str(), *c, rec);
+        profile.start();
+    }
+
+    LdmsForwarder(const char* fname)
+        : filename { fname }
+    { }
+
+public:
+
+    static void create(Caliper* c, Channel* channel) {
+        ConfigSet cfg = channel->config().init("ldms", s_configdata);
+
+        LdmsForwarder* instance = new LdmsForwarder(cfg.get("filename").to_string().c_str());
+
+        channel->events().post_init_evt.connect(
+            [instance](Caliper* c, Channel* channel){
+                instance->post_init(c, channel);
+            });
+        channel->events().snapshot.connect(
+            [instance](Caliper* c, Channel* channel, int scope, const SnapshotRecord* info, SnapshotRecord* snapshot){
+                instance->snapshot(c, channel);
+            });
+        channel->events().finish_evt.connect(
+            [instance](Caliper* c, Channel* chn){
+                delete instance;
+            });
+
+    }
+};
+
+const ConfigSet::Entry LdmsForwarder::s_configdata[] = {
+    { "filename", CALI_TYPE_STRING, "stdout",
+      "Output file name.",
+      "Output file name."
+    },
+    ConfigSet::Terminator
+};
+
+} // namespace [anonymous]
+
+namespace cali
+{
+
+CaliperService ldms_service { "ldms", ::LdmsForwarder::create };
+
+}
