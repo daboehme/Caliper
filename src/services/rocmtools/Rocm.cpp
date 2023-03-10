cd5e0f5a4cf4601448ef5a0491383204768ae33e
David Boehme <boehme3@llnl.gov>
2023-03-09 18:24:20 -0800

    Make rocmtools configurable

--- src/services/rocmtools/Rocm.cpp
+++ src/services/rocmtools/Rocm.cpp
@@ -61,7 +61,8 @@ class RocmService
     Attribute m_kernel_name_attr;
     Attribute m_begin_ts_attr;
     Attribute m_end_ts_attr;
-    Attribute m_duration_attr;
+    Attribute m_activity_duration_attr;
+    Attribute m_api_duration_attr;
     Attribute m_host_timestamp_attr;
     Attribute m_host_duration_attr;
     Attribute m_host_starttime_attr;
@@ -72,6 +73,14 @@ class RocmService
 
     std::map< uint64_t, std::vector<Entry> > m_correlation_map;
     std::mutex m_correlation_map_mutex;
+
+    bool      m_trace_api;
+    bool      m_trace_activities;
+    bool      m_correlate_contex;
+    bool      m_record_host_timestamps;
+
+    unsigned  m_num_flushes;
+    unsigned  m_num_records;
 
     Channel*  m_channel;
 
@@ -96,8 +105,11 @@ class RocmService
         m_end_ts_attr =
             c->create_attribute("rocm.endtime", CALI_TYPE_UINT,
                 CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS);
-        m_duration_attr =
+        m_activity_duration_attr =
             c->create_attribute("rocm.activity.duration", CALI_TYPE_UINT,
+                CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS | CALI_ATTR_AGGREGATABLE);
+        m_api_duration_attr =
+            c->create_attribute("rocm.api.duration", CALI_TYPE_UINT,
                 CALI_ATTR_ASVALUE | CALI_ATTR_SKIP_EVENTS | CALI_ATTR_AGGREGATABLE);
         m_correlation_attr =
             c->create_attribute("rocm.correlation_id", CALI_TYPE_UINT,
@@ -167,7 +179,7 @@ class RocmService
                     m_api_attr,
                     m_begin_ts_attr,
                     m_end_ts_attr,
-                    m_duration_attr
+                    m_api_duration_attr
                 };
                 const Variant data[5] = {
                     Variant(CALI_TYPE_STRING, "hip_api", 7),
@@ -181,9 +193,12 @@ class RocmService
                 Caliper c;
 
                 c.pull_context(m_channel, CALI_SCOPE_THREAD | CALI_SCOPE_PROCESS, rec.builder());
-                auto view = rec.view();
-                push_correlation(trec.correlation_id.value,
-                    std::vector<Entry> { view.begin(), view.end() });
+
+                if (m_correlate_contex) {
+                    auto view = rec.view();
+                    push_correlation(trec.correlation_id.value,
+                        std::vector<Entry> { view.begin(), view.end() });
+                }
 
                 c.make_record(5, attr, data, rec.builder());
                 m_channel->events().process_snapshot(&c, m_channel, SnapshotView(), rec.view());
@@ -194,7 +209,6 @@ class RocmService
     void handle_trace_record(rocmtools_record_tracer_t trec, rocmtools_session_id_t session) {
         std::string activity_name;
         std::string kernel_name;
-        std::string function_name;
 
         if (trec.domain == ACTIVITY_DOMAIN_HIP_OPS) {
             const char* name = reinterpret_cast<const char*>(trec.api_data_handle.handle);
@@ -210,21 +224,6 @@ class RocmService
                 activity_name = cptr;
         }
 
-        if (trec.domain == ACTIVITY_DOMAIN_HIP_API) {
-            size_t function_name_size = 0;
-            rocmtools_query_hip_tracer_api_data_info_size(
-                session, ROCMTOOLS_HIP_FUNCTION_NAME, trec.api_data_handle,
-                trec.operation_id, &function_name_size);
-
-            if (function_name_size > 1) {
-                char* cptr = nullptr;
-                rocmtools_query_hip_tracer_api_data_info(
-                    session, ROCMTOOLS_HIP_FUNCTION_NAME, trec.api_data_handle,
-                    trec.operation_id, &cptr);
-                function_name = cptr;
-            }
-        }
-
         Caliper c;
 
         cali::Node* node = nullptr;
@@ -234,8 +233,6 @@ class RocmService
             node = c.make_tree_entry(m_domain_attr, Variant(domain_name));
         if (activity_name.size() > 0)
             node = c.make_tree_entry(m_activity_attr, Variant(activity_name.c_str()), node);
-        if (function_name.size() > 0)
-            node = c.make_tree_entry(m_api_attr, Variant(function_name.c_str()), node);
         if (kernel_name.size() > 0)
             node = c.make_tree_entry(m_kernel_name_attr, Variant(kernel_name.c_str()), node);
 
@@ -248,10 +245,12 @@ class RocmService
 
             rec.builder().append(m_begin_ts_attr, cali_make_variant_from_uint(trec.timestamps.begin.value));
             rec.builder().append(m_end_ts_attr, cali_make_variant_from_uint(trec.timestamps.end.value));
-            rec.builder().append(m_duration_attr, cali_make_variant_from_uint(duration));
+            rec.builder().append(m_activity_duration_attr, cali_make_variant_from_uint(duration));
 
-            auto ctx = pop_correlation(trec.correlation_id.value);
-            rec.builder().append(ctx.size(), ctx.data());
+            if (m_correlate_contex) {
+                auto ctx = pop_correlation(trec.correlation_id.value);
+                rec.builder().append(ctx.size(), ctx.data());
+            }
 
             m_channel->events().process_snapshot(&c, m_channel, SnapshotView(), rec.view());
         }
@@ -275,6 +274,9 @@ class RocmService
             }
         }
 
+        m_num_records += count;
+        ++m_num_flushes;
+
         Log(1).stream() << m_channel->name() << ": rocmtools: flushed " << count << " records\n";
     }
 
@@ -295,10 +297,21 @@ class RocmService
             ACTIVITY_DOMAIN_HIP_OPS
         };
 
+        size_t f_count = 2;
+        rocmtools_filter_data_t f_data { filters };
+
+        if (m_trace_api && !m_trace_activities) {
+            f_count = 1;
+        } else if (!m_trace_api && m_trace_activities) {
+            f_data = rocmtools_filter_data_t { &filters[1] };
+            f_count = 1;
+        } else if (!m_trace_api && !m_trace_activities) {
+            f_count = 0;
+        }
+
         rocmtools_filter_id_t api_filter_id;
         ret = rocmtools_create_filter(m_session_id, ROCMTOOLS_API_TRACE,
-            rocmtools_filter_data_t { filters }, 2,
-            &api_filter_id, rocmtools_filter_property_t {});
+            f_data, f_count, &api_filter_id, rocmtools_filter_property_t {});
         if (ret != ROCMTOOLS_STATUS_SUCCESS) {
             print_rocm_error(channel, "rocmtools_create_filter()", ret);
             return;
@@ -319,15 +332,17 @@ class RocmService
             return;
         }
 
-        rocmtools_filter_id_t kernel_filter_id;
-        ret = rocmtools_create_filter(m_session_id, ROCMTOOLS_DISPATCH_TIMESTAMPS_COLLECTION,
-                rocmtools_filter_data_t {}, 0, &kernel_filter_id, rocmtools_filter_property_t {});
-        if (ret != ROCMTOOLS_STATUS_SUCCESS) {
-            print_rocm_error(channel, "rocmtools_create_filter", ret);
-        }
-        ret = rocmtools_set_filter_buffer(m_session_id, kernel_filter_id, m_buffer_id);
-        if (ret != ROCMTOOLS_STATUS_SUCCESS) {
-            print_rocm_error(channel, "rocmtools_filter_set_buffer", ret);
+        if (m_trace_activities) {
+            rocmtools_filter_id_t kernel_filter_id;
+            ret = rocmtools_create_filter(m_session_id, ROCMTOOLS_DISPATCH_TIMESTAMPS_COLLECTION,
+                    rocmtools_filter_data_t {}, 0, &kernel_filter_id, rocmtools_filter_property_t {});
+            if (ret != ROCMTOOLS_STATUS_SUCCESS) {
+                print_rocm_error(channel, "rocmtools_create_filter", ret);
+            }
+            ret = rocmtools_set_filter_buffer(m_session_id, kernel_filter_id, m_buffer_id);
+            if (ret != ROCMTOOLS_STATUS_SUCCESS) {
+                print_rocm_error(channel, "rocmtools_filter_set_buffer", ret);
+            }
         }
 
         Log(1).stream() << channel->name() << ": rocmtools: API tracing session initialized\n";
@@ -358,16 +373,18 @@ class RocmService
             return;
         }
 
-        rocmtools_timestamp_t timestamp { 0 };
-        rocmtools_get_timestamp(&timestamp);
+        if (m_record_host_timestamps) {
+            rocmtools_timestamp_t timestamp { 0 };
+            rocmtools_get_timestamp(&timestamp);
 
-        c->set(m_host_starttime_attr, cali_make_variant_from_uint(timestamp.value));
-        c->set(m_host_timestamp_attr, cali_make_variant_from_uint(timestamp.value));
+            c->set(m_host_starttime_attr, cali_make_variant_from_uint(timestamp.value));
+            c->set(m_host_timestamp_attr, cali_make_variant_from_uint(timestamp.value));
 
-        channel->events().snapshot.connect(
-            [](Caliper* c, Channel*, int, SnapshotView, SnapshotBuilder& rec){
-                s_instance->snapshot_cb(c, rec);
-            });
+            channel->events().snapshot.connect(
+                [](Caliper* c, Channel*, int, SnapshotView, SnapshotBuilder& rec){
+                    s_instance->snapshot_cb(c, rec);
+                });
+        }
 
         Log(1).stream() << channel->name() << ": rocmtools: session started\n";
     }
@@ -390,12 +407,27 @@ class RocmService
 
     void finish_cb(Caliper* c, Channel* channel) {
         rocmtools_finalize();
+
+        Log(1).stream() << channel->name() << ": rocmtools: flushed "
+            << m_num_records << " records in " << m_num_flushes << " flushes\n";
     }
 
     RocmService(Caliper* c, Channel* channel)
-        : m_channel { channel }
+        : m_num_flushes { 0 },
+          m_num_records { 0 },
+          m_channel     { channel }
     {
         create_attributes(c);
+
+        auto config = services::init_config_from_spec(channel->config(), s_spec);
+
+        m_trace_api = config.get("trace_api").to_bool();
+        m_trace_activities =
+            config.get("trace_activities").to_bool();
+        m_correlate_contex =
+            config.get("correlate_context").to_bool() && m_trace_activities && m_trace_api;
+        m_record_host_timestamps =
+            config.get("record_host_timestamps").to_bool();
     }
 
 public:
@@ -449,17 +481,17 @@ const char* RocmService::s_spec = R"json(
             "description": "Enable ROCm GPU activity tracing",
             "value": "true"
         },
-        {   "name": "record_kernel_names",
+        {   "name": "trace_api",
             "type": "bool",
-            "description": "Record kernel names when activity tracing is enabled",
-            "value": "false"
+            "description": "Enable HIP API tracing",
+            "value": "true"
         },
-        {   "name": "snapshot_duration",
+        {   "name": "correlate_context",
             "type": "bool",
-            "description": "Record duration of host-side activities using ROCm timestamps",
-            "value": "false"
+            "description": "Correlate HIP activities to host-side context",
+            "value": "true"
         },
-        {   "name": "snapshot_timestamps",
+        {   "name": "record_host_timestamps",
             "type": "bool",
             "description": "Record host-side timestamps with ROCm",
             "value": "false"
